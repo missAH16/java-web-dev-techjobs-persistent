09f1275aa2fa47db1145068be22f44e065c2bcc6
missAH16 <missahiatt@gmail.com>
2020-11-10 10:47:56 -0600

    .omg.commit...

--- queries.sql
+++ queries.sql
@@ -5,3 +5,6 @@ SELECT * FROM  employer WHERE (location = "St. Louis City")
 ## Part 3: Test it with SQL
 DROP TABLE job
 ## Part 4: Test it with SQL
+SELECT * FROM skill
+WHERE id IN (SELECT skills_id FROM job_skills WHERE skills_id IS NOT NULL)
+ORDER BY name ASC;
